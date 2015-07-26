337c618396ac44749deb4b90c77a9b7399833126
mongoose11235813 <mongoose11235813@gamil.com>
2015-07-26 23:40:09 +0300

    Der Download erfolgt auf eigene Gefahr.

---
+++ CommandSeatIconFix/IconFixAddon.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace CommandSeatIconFix
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class IconFixAddon: MonoBehaviour
+    {
+        const string brokenShaderName = "KSP/Alpha/Cutoff";
+        const string fixShaderName = "KSP/Alpha/Cutoff Bumped";
+
+        private void fixSader (AvailablePart availablePart, string brokenShaderName, Shader fixShader)
+        {
+            // Debug.Log ("IconFixAddon : part: " + availablePart.name);
+            foreach (Renderer r in availablePart.iconPrefab.GetComponentsInChildren<Renderer>(true)) {
+                foreach (Material m in r.materials) {
+                    if (m.shader.name == brokenShaderName) {
+                        Debug.Log ("IconFixAddon: fixing icon of: " + availablePart.name);
+                        m.shader = fixShader;
+                    }
+                }
+            }
+        }
+
+        public void Awake()
+        {
+            // Debug.Log ("IconFixAddon: Awake()");
+            if (!PartLoader.Instance || !PartLoader.Instance.IsReady ()) {
+                Debug.LogError ("IconFixAddon: can't access part loader");
+                return;
+            }
+            Shader fixShader = Shader.Find (fixShaderName);
+            if (!fixShader) {
+                Debug.LogError ("IconFixAddon: can't get the fix shader");
+                return;
+            }
+            foreach (AvailablePart availablePart in PartLoader.LoadedPartsList) {
+                fixSader (availablePart, brokenShaderName, fixShader);
+            }
+        }
+    }
+}
+
